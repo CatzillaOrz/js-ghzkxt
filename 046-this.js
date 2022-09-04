0f063196597c4c62842172c27ad4982cb696f7c1
CatzillaOrz <catzillaorz@outlook.com>
2022-09-04 22:19:26 +0800

    1 file modified

--- 046-this.js
+++ 046-this.js
@@ -1,3 +1,41 @@
 /**
  * @this is the object that the function is a property of
  */
+
+// snippets #1
+
+const obj = {
+  name: 'Billy',
+  sing() {
+    return 'lalala' + this.name;
+  },
+
+  singAgain() {
+    this.sing() + '!';
+  },
+};
+
+
+/**
+ * @this the bennifits : execute same code for multiple objects
+ */
+
+function importantPerson(){
+  console.log(this.name +. this.name + '!');
+}
+
+const name = 'Sunny'
+
+const obj1 = {
+  name: 'Cassy',
+  importantPerson: importantPerson
+}
+
+const obj2 = {
+  name: 'Jacob',
+  importantPerson: importantPerson
+}
+
+importantPerson() // 'Sunny'
+obj1.importantPerson() // 'Cassy'
+obj1.importantPerson() // 'Jacob'

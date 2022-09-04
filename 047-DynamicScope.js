d3928bf29fcd4b1fe3e9e719929a440bac5292f6
CatzillaOrz <catzillaorz@outlook.com>
2022-09-04 22:47:47 +0800

    1 file modified

--- 047-DynamicScope.js
+++ 047-DynamicScope.js
@@ -21,3 +21,23 @@ a();
 // a > Window
 // b > Window
 // c > {hi: ƒ}
+
+/**
+ * @this
+ * In javascript our lexical scope(available data + variables where the funcion was defined) determines our available variables. Not where the function is called(dynamic scope)
+ */
+
+const obj = {
+  name: 'Billy',
+  sing() {
+    console.log('a', this);
+    var anthoerFunc = function () {
+      console.log('b', this);
+    };
+    anotherFunc();
+  },
+};
+
+obj.sing();
+// a {name: "Billy", sing: ƒ}
+// b Window

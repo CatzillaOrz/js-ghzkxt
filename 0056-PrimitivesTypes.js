cea379354facae58366ab7b4927544ff6ddd27bb
catzillaorz <catzillaorz@outlook.com>
2022-09-06 11:23:31 +0800

    feat: Primitives PrimitivesTypes

---
+++ 0056-PrimitivesTypes.js
@@ -0,0 +1,12 @@
+// Primitive
+typeof 5;
+typeof true;
+typeof 'To be or not to be';
+typeof undefined;
+typeof null;
+typeof Symbol('Just me');
+
+// Noe-primitive
+typeof {};
+typeof [];
+typeof function () {};

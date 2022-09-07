f619a852eb01b983f41629fb4adda265472d7c58
catzillaorz <catzillaorz@outlook.com>
2022-09-07 16:55:26 +0800

    feat 4PilesOfOOP

---
+++ 108-4PilesOfOOP.js
@@ -0,0 +1,17 @@
+/**
+ * @description 4 piles of oop
+ * #1 encapsulation
+ * #2 abstration
+ * #3 inheritance
+ * #4 polymorphism
+ */
+
+// Benifit
+
+/**
+ * #1 Clear + Understandable
+ * #2 Easy to Extend
+ * #3 Easy to Maintain
+ * #4 Memory Efficient
+ * #5 Dry
+ */

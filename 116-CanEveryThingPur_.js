d4488a9f6085077a13aeb2cf0544d35c8c536a84
catzillaorz <catzillaorz@outlook.com>
2022-09-07 17:42:09 +0800

    feat: Idempotence

---
+++ 116-CanEveryThingPur_.js
@@ -0,0 +1,13 @@
+/**
+ * @description As a matter of fact, input out put is a side of effect that is communicating with the outside world in anyway is not pure
+ * # with just pure functions, which are just functions that do something on the inside and outside world knows nothing about is philoosophically do anything
+ * # because the program cannot exist without side effects.
+ * # Pure function:
+ * # 1 Task
+ * # Predictable
+ * # return statement
+ * # Composable
+ * # Pure
+ * # Immutable State
+ * # No shared state
+ */
---
+++ 117-Idempotence.js
@@ -0,0 +1,5 @@
+// Idempotence:
+
+Math.abs(Math.abs(-50));
+// call it inside itself over and over again
+// predictable always
